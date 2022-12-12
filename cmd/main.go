fe172ecc9a8981339f163143980bbe87883c60d0
GarretTomlin <garrettomlin808@gmail.com>
2022-12-11 21:27:04 -0500

    first commits

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"JSE_API/pkg/models"
-	"github.com/gocolly/colly"
 	"github.com/gocolly/colly/queue"
 	ds"JSE_API/pkg/configs"
 )
+
+var Storage *models.UrlStorage
 
 func main(){
 	  // Connect to the database
@@ -13,28 +14,19 @@ func main(){
 	  if err != nil {
 		 panic(err)
 	}
-
-
-	  // Create a new MyStorage instance
-	  storage := &models.UrlStorage{Db: db, Cache: make(map[string]bool)}
-
-	  // Create a new colly collector
-	  c := colly.NewCollector(
-		// Use multiple threads to improve scraping performance
-		colly.Async(true),
-	  )
-
+	  // Create a new Storage instance
+	  Storage := &models.UrlStorage{Db: db, Cache: make(map[string]bool)}
 
 	  q, _ := queue.New(
 		// Use 10 threads for scraping
 		10,
 		// Use the MyStorage instance as the storage backend for the URL queue
-		storage,
+		Storage,
 	  )
 
 
 
   // Start scraping
-  q.Run(c)
+  q.Run(ds.C)
 
 }
--- pkg/controllers/tradeSummary.go
+++ pkg/controllers/tradeSummary.go
@@ -1,10 +1,13 @@
 package controllers
 
 import (
+	store "JSE_API/cmd"
 	c "JSE_API/pkg/configs"
 	"github.com/gocolly/colly"
 	"github.com/julienschmidt/httprouter"
+	"log"
 	"net/http"
+	"regexp"
 )
 
 type TradeSummary struct {
@@ -43,6 +46,37 @@ func (s *TradeSummary) GetStockAdvancing(w http.ResponseWriter, r *http.Request,
 
 		summary = append(summary, allData)
 	})
+
+	// Create a list of URL filters as regular expressions
+	var urlFilters = []*regexp.Regexp{
+		regexp.MustCompile(`example\.com`),
+		regexp.MustCompile(`example\.org`),
+	}
+
+	// Get the URL from the store
+	u, err := store.Storage.Get()
+	if err != nil {
+		// Handle the error by logging it or taking some other action
+		log.Println(err)
+	} else {
+		// Flag to track whether the URL was skipped by a filter
+		skipped := false
+
+		// Check if the URL matches any of the filters
+		for _, filter := range urlFilters {
+			if filter.MatchString(u.String()) {
+				// If the URL matches a filter, skip it and set the skipped flag to true
+				skipped = true
+				continue
+			}
+		}
+
+		// Check if the URL was not skipped by any of the filters
+		if !skipped {
+			// Visit the URL by calling the String method on the u variable
+			c.C.Visit(u.String())
+		}
+	}
 
 	// Send the JSON response
 	SendJson(w, http.StatusOK, summary)
